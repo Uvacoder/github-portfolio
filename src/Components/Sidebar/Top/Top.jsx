48b1d550e2b1a97db0c55f032af1b9e7314f7bc4
hima_khaitan <himanshukhaitan108@gmail.com>
2021-05-18 03:59:50 +0530

    Upper Sidebar Configured!

--- src/Components/Sidebar/Top/Top.jsx
+++ src/Components/Sidebar/Top/Top.jsx
@@ -1,9 +1,14 @@
+// IMPORT CSS
 import "./Top.css";
+
+// IMPORT IMAGE
+import img1 from "./img/img.jpg";
 
 const Top = () => {
     return(
         <div className="top">
-
+            <img src={img1} alt="" className="top-image"/>
+            <div className="top-abs"> <p>💯</p> <span>Working</span></div>
         </div>
     )
 }
