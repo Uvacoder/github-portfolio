fa9c8852f35a7542c087bebc2230880f1dd2ad05
hima_khaitan <himanshukhaitan108@gmail.com>
2021-05-19 01:24:50 +0530

    Routes Configured!

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,18 +1,26 @@
-// COMPONENTS IMPORT
+// COMPONENTS IMPORTS
 import Sidebar from "../Sidebar/Sidebar";
+import Home from "./Routes/Home/Home";
+
+// MoDULE IMPORTS
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // CSS IMPORTS
 import "./Main.css";
 
 // FUNCTION DECLERATION
 const Main = () => {
-    return (
-        <div className="main">
-            <Sidebar/>
-            <div>hello</div>
-        </div>
-    )
-}
+  return (
+    <div className="main">
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 // FUNCTION EXPORT
 export default Main;
---
+++ src/Components/Main/Routes/Home/Home.jsx
@@ -0,0 +1,18 @@
+// CSS IMPORT
+import "./Home.css";
+
+// COMPONENTS IMPORT
+import Container from "../../Utils/Container";
+
+// FUNCTION DECLERATION
+const Home = () => {
+  return (
+    <Container>
+      <div className="home">Hello MF</div>
+    </Container>
+  );
+};
+
+// EXPORT
+
+export default Home;
